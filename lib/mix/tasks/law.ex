2ca51a31d2879dd9c5966e8287d19545d2638a3a
Tkachuk Ilya <tim@MacBook-Pro-Tkachuk.local>
2017-08-05 00:55:40 +0300

    add credo, coverex, dialyzer support in simple and umbrella projects

--- lib/mix/tasks/law.ex
+++ lib/mix/tasks/law.ex
@@ -70,12 +70,14 @@ defmodule Mix.Tasks.Law do
 
     create_file "README.md",  readme_template(assigns)
     create_file ".gitignore", gitignore_text()
-    create_file ".coverex_ignore.exs", coverex_ignore_template(assigns)
 
     if in_umbrella?() do
       create_file "mix.exs", mixfile_apps_template(assigns)
     else
       create_file "mix.exs", mixfile_template(assigns)
+      create_file ".credo.exs", credo_template(assigns)
+      create_file ".coverex_ignore.exs", coverex_ignore_template(assigns)
+      create_file ".dialyzer_ignore", dialyzer_ignore_text()
     end
 
     create_directory "config"
@@ -114,6 +116,9 @@ defmodule Mix.Tasks.Law do
     create_file ".gitignore", gitignore_text()
     create_file "README.md", readme_template(assigns)
     create_file "mix.exs", mixfile_umbrella_template(assigns)
+    create_file ".credo.exs", credo_template(assigns)
+    create_file ".coverex_ignore.exs", coverex_ignore_template(assigns)
+    create_file ".dialyzer_ignore", dialyzer_ignore_text()
 
     create_directory "apps"
 
@@ -225,12 +230,18 @@ defmodule Mix.Tasks.Law do
   *.ez
   """
 
+  embed_text :dialyzer_ignore, """
+  Any dialyzer's error output lines putted to this text file will be completely ignored by dialyzer's type checks.
+  Please not abuse this file, type checks are VERY important.
+  Use this file just in case of bad 3rd party auto-generated code.
+  """
+
   embed_template :coverex_ignore, """
   [
     #
     # Modules from this list will be completely ignored by coverex tool.
-    # Please not abuse this file.
-    # Use it just in case of bad 3rd party auto-generated code.
+    # Please not abuse this file, test coverage is VERY important.
+    # Use this file just in case of bad 3rd party auto-generated code.
     #
     # Examples:
     #
@@ -240,10 +251,159 @@ defmodule Mix.Tasks.Law do
   ]
   """
 
+  embed_template :credo, """
+  %{
+    #
+    # You can have as many configs as you like in the `configs:` field.
+    configs: [
+      %{
+        #
+        # Run any exec using `mix credo -C <name>`. If no exec name is given
+        # "default" is used.
+        #
+        name: "default",
+        #
+        # These are the files included in the analysis:
+        files: %{
+          #
+          # You can give explicit globs or simply directories.
+          # In the latter case `**/*.{ex,exs}` will be used.
+        #
+          included: ["lib/", "src/", "web/", "apps/"],
+          excluded: [~r"/_build/", ~r"/deps/"]
+        },
+        #
+        # If you create your own checks, you must specify the source files for
+        # them here, so they can be loaded by Credo before running the analysis.
+        #
+        requires: [],
+        #
+        # Credo automatically checks for updates, like e.g. Hex does.
+        # You can disable this behaviour below:
+        #
+        check_for_updates: true,
+        #
+        # If you want to enforce a style guide and need a more traditional linting
+        # experience, you can change `strict` to `true` below:
+        #
+        strict: true,
+        #
+        # If you want to use uncolored output by default, you can change `color`
+        # to `false` below:
+        #
+        color: true,
+        #
+        # You can customize the parameters of any check by adding a second element
+        # to the tuple.
+        #
+        # To disable a check put `false` as second element:
+        #
+        #     {Credo.Check.Design.DuplicatedCode, false}
+        #
+        checks: [
+          {Credo.Check.Consistency.ExceptionNames},
+          {Credo.Check.Consistency.LineEndings},
+          {Credo.Check.Consistency.ParameterPatternMatching},
+          {Credo.Check.Consistency.SpaceAroundOperators},
+          {Credo.Check.Consistency.SpaceInParentheses},
+          {Credo.Check.Consistency.TabsOrSpaces},
+
+          # For some checks, like AliasUsage, you can only customize the priority
+          # Priority values are: `low, normal, high, higher`
+          #
+          {Credo.Check.Design.AliasUsage, priority: :low, exit_status: 0},
+
+          # For others you can set parameters
+
+          # If you don't want the `setup` and `test` macro calls in ExUnit tests
+          # or the `schema` macro in Ecto schemas to trigger DuplicatedCode, just
+          # set the `excluded_macros` parameter to `[:schema, :setup, :test]`.
+          #
+          {Credo.Check.Design.DuplicatedCode, excluded_macros: []},
+
+          # You can also customize the exit_status of each check.
+          # If you don't want TODO comments to cause `mix credo` to fail, just
+          # set this value to 0 (zero).
+          #
+          {Credo.Check.Design.TagTODO, priority: :low, exit_status: 0},
+          {Credo.Check.Design.TagFIXME, priority: :low, exit_status: 0},
+
+          {Credo.Check.Readability.FunctionNames},
+          {Credo.Check.Readability.LargeNumbers},
+          {Credo.Check.Readability.MaxLineLength, priority: :normal, max_length: 120, exit_status: 0},
+          {Credo.Check.Readability.ModuleAttributeNames},
+          {Credo.Check.Readability.ModuleDoc, false},
+          {Credo.Check.Readability.ModuleNames, priority: :high},
+          {Credo.Check.Readability.ParenthesesOnZeroArityDefs, priority: :high},
+          {Credo.Check.Readability.ParenthesesInCondition, priority: :high},
+          {Credo.Check.Readability.PredicateFunctionNames, priority: :high},
+          {Credo.Check.Readability.PreferImplicitTry, false},
+          {Credo.Check.Readability.RedundantBlankLines, priority: :low},
+          {Credo.Check.Readability.StringSigils},
+          {Credo.Check.Readability.TrailingBlankLine},
+          {Credo.Check.Readability.TrailingWhiteSpace},
+          {Credo.Check.Readability.VariableNames},
+          {Credo.Check.Readability.Semicolons},
+          {Credo.Check.Readability.SpaceAfterCommas, priority: :low},
+
+          {Credo.Check.Refactor.DoubleBooleanNegation, priority: :high},
+          {Credo.Check.Refactor.CondStatements},
+          {Credo.Check.Refactor.CyclomaticComplexity, priority: :high, exit_status: 2, max_complexity: 12},
+          {Credo.Check.Refactor.FunctionArity},
+          {Credo.Check.Refactor.LongQuoteBlocks},
+          {Credo.Check.Refactor.MatchInCondition},
+          {Credo.Check.Refactor.NegatedConditionsInUnless, priority: :high, exit_status: 2},
+          {Credo.Check.Refactor.NegatedConditionsWithElse, priority: :normal},
+          {Credo.Check.Refactor.Nesting, max_nesting: 3, priority: :high, exit_status: 2},
+          {Credo.Check.Refactor.PipeChainStart, false},
+          {Credo.Check.Refactor.UnlessWithElse, priority: :higher, exit_status: 2},
+
+          {Credo.Check.Warning.BoolOperationOnSameValues, priority: :high, exit_status: 2},
+          {Credo.Check.Warning.IExPry, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.IoInspect, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.LazyLogging, false},
+          {Credo.Check.Warning.OperationOnSameValues, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.OperationWithConstantResult, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedEnumOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedFileOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedKeywordOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedListOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedPathOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedRegexOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedStringOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.UnusedTupleOperation, priority: :higher, exit_status: 2},
+          {Credo.Check.Warning.RaiseInsideRescue, priority: :higher, exit_status: 2},
+
+          # Controversial and experimental checks (opt-in, just remove `, false`)
+          #
+          {Credo.Check.Refactor.ABCSize, false},
+          {Credo.Check.Refactor.AppendSingleItem, priority: :normal},
+          {Credo.Check.Refactor.VariableRebinding, priority: :normal},
+          {Credo.Check.Warning.MapGetUnsafePass, priority: :high, exit_status: 0},
+          {Credo.Check.Consistency.MultiAliasImportRequireUse, priority: :normal, exit_status: 0},
+
+          # Deprecated checks (these will be deleted after a grace period)
+          #
+          {Credo.Check.Readability.Specs, false},
+          {Credo.Check.Warning.NameRedeclarationByAssignment, false},
+          {Credo.Check.Warning.NameRedeclarationByCase, priority: :normal},
+          {Credo.Check.Warning.NameRedeclarationByDef, priority: :normal},
+          {Credo.Check.Warning.NameRedeclarationByFn, priority: :normal},
+
+          # Custom checks can be created using `mix credo.gen.check`.
+          #
+        ]
+      }
+    ]
+  }
+  """
+
   embed_template :mixfile, """
   defmodule <%= @mod %>.Mixfile do
     use Mix.Project
     def project do
+      {coverex_ignore_modules, []} = Code.eval_file(".coverex_ignore.exs")
+      true = is_list(coverex_ignore_modules)
       [
         app: :<%= @app %>,
         version: "0.1.0",
@@ -255,9 +415,10 @@ defmodule Mix.Tasks.Law do
           tool: Coverex.Task,
           output: "./cover",
           coveralls: true,
-          ignore_modules: Code.eval_file(".coverex_ignore.exs")
+          ignore_modules: coverex_ignore_modules
         ],
         dialyzer: [ignore_warnings: ".dialyzer_ignore"],
+        elixirc_paths: elixirc_paths(Mix.env),
         deps: deps()
       ]
     end
@@ -277,6 +438,10 @@ defmodule Mix.Tasks.Law do
         # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       ]
     end
+    # Specifies which paths to compile per environment.
+    defp elixirc_paths(:test), do: ["test/support"] ++ elixirc_paths()
+    defp elixirc_paths(_), do: elixirc_paths()
+    defp elixirc_paths, do: ["lib"]
   end
   """
 
@@ -284,6 +449,8 @@ defmodule Mix.Tasks.Law do
   defmodule <%= @mod %>.Mixfile do
     use Mix.Project
     def project do
+      {coverex_ignore_modules, []} = Code.eval_file("../../.coverex_ignore.exs")
+      true = is_list(coverex_ignore_modules)
       [
         app: :<%= @app %>,
         version: "0.1.0",
@@ -292,7 +459,16 @@ defmodule Mix.Tasks.Law do
         deps_path: "../../deps",
         lockfile: "../../mix.lock",
         elixir: "~> <%= @version %>",
+        build_embedded: false,
+        consolidate_protocols: true,
         start_permanent: Mix.env == :prod,
+        test_coverage: [
+          tool: Coverex.Task,
+          output: "../../cover",
+          coveralls: true,
+          ignore_modules: coverex_ignore_modules
+        ],
+        elixirc_paths: elixirc_paths(Mix.env),
         deps: deps()
       ]
     end
@@ -310,6 +486,10 @@ defmodule Mix.Tasks.Law do
         # {:sibling_app_in_umbrella, in_umbrella: true},
       ]
     end
+    # Specifies which paths to compile per environment.
+    defp elixirc_paths(:test), do: ["test/support"] ++ elixirc_paths()
+    defp elixirc_paths(_), do: elixirc_paths()
+    defp elixirc_paths, do: ["lib"]
   end
   """
 
@@ -319,8 +499,11 @@ defmodule Mix.Tasks.Law do
     def project do
       [
         apps_path: "apps",
+        build_embedded: false,
+        consolidate_protocols: true,
         start_permanent: Mix.env == :prod,
-        deps: deps()
+        deps: deps(),
+        dialyzer: [ignore_warnings: ".dialyzer_ignore"],
       ]
     end
     # Dependencies listed here are available only for this
@@ -329,7 +512,11 @@ defmodule Mix.Tasks.Law do
     #
     # Run "mix help deps" for examples and options.
     defp deps do
-      []
+      [
+        {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+        {:coverex, "1.4.13", only: [:dev, :test], runtime: false},
+        {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      ]
     end
   end
   """
