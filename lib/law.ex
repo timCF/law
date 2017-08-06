c9cda3ad4872f29f9f955a88a6233d04ea415518
Tkachuk Ilya <tim@MacBook-Pro-Tkachuk.local>
2017-08-06 16:54:28 +0300

    make pre-commit hook symbolic link on compile time

--- lib/law.ex
+++ lib/law.ex
@@ -2,14 +2,18 @@ defmodule Law do
   @moduledoc """
   Documentation for Law.
   """
-  project_top_path = Mix.Project.deps_path() |> Path.join("..") |> IO.inspect
-  git_hooks_path = Path.join(project_top_path, ".git/hooks") |> IO.inspect
+  project_top_path = Mix.Project.deps_path() |> Path.join("..")
+  git_hooks_path = Path.join(project_top_path, ".git/hooks")
   case File.exists?(git_hooks_path) do
     true ->
-      #
-      # TODO
-      #
-      :ok
+      pre_commit_hook_path = Path.join(git_hooks_path, "pre-commit")
+      case  project_top_path
+            |> Path.join("pre-commit")
+            |> File.ln_s(pre_commit_hook_path) do
+        :ok -> Mix.shell.info("pre-commit hook #{pre_commit_hook_path} created")
+        {:error, :eexist} -> Mix.shell.info("pre-commit hook #{pre_commit_hook_path} already exists, do nothing")
+        error -> raise("Can not create symbolic link for pre-commit hook #{pre_commit_hook_path} because of error #{inspect error}")
+      end
     false ->
       raise("It seems path #{git_hooks_path} is not exist. To keep the Law, your Elixir project should be in git repository.")
   end
