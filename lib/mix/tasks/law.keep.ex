620576e4e9464f42b2997aefcb58f4548138bd11
Tkachuk Ilya <tim@MacBook-Pro-Tkachuk.local>
2017-08-06 17:31:27 +0300

    fix compile tasks validation

--- lib/mix/tasks/law.keep.ex
+++ lib/mix/tasks/law.keep.ex
@@ -1,10 +1,11 @@
 defmodule Mix.Tasks.Law.Keep do
   use Mix.Task
+  @taskres [:ok, :noop]
 
   def run(_) do
     :ok = Mix.env(:test)
-    :ok = Mix.Task.run("deps.compile")
-    :ok = Mix.Task.run("compile", ["--warnings-as-errors"])
+    true = Mix.Task.run("deps.compile") in @taskres
+    true = Mix.Task.run("compile", ["--warnings-as-errors"]) in @taskres
     :ok = Mix.Task.run("credo", ["--strict"])
     :ok = Mix.Task.run("test", ["--cover"])
     :ok = Mix.Task.run("dialyzer", ["--halt-exit-status"])
